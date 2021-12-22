e195e4c6b83b0450833f8a8b19c5a659052b1573
putraperdana95 <putra.perdana7@gmail.com>
2021-12-22 13:01:54 +0700

    membuat file mahasiswa

---
+++ pertemuan9/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+
+<body>
+
+    <h1>Daftar Mahasiswa</h1>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+
+        <tr>
+            <th>No. Urut</th>
+            <th>Aksi</th>
+            <th>Foto Mahasiswa</th>
+            <th>NIM</th>
+            <th>Nama Lengkap</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+        </tr>
+
+        <tr>
+            <td>1</td>
+            <td>
+                <a href="">Ubah</a> |
+                <a href="">Hapus</a>
+            </td>
+            <td><img src="" alt=""></td>
+        </tr>
+
+    </table>
+</body>
+
+</html>
