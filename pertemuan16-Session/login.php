c8597a6ab215c7d1d64bc432f9c117a441c2e34a
putraperdana95 <putra.perdana7@gmail.com>
2022-01-03 09:00:13 +0700

    login dan logout selesai

--- pertemuan16-Session/login.php
+++ pertemuan16-Session/login.php
@@ -1,4 +1,11 @@
 <?php
+session_start();
+
+if (isset($_SESSION["login"])) {
+    header("Location: index.php");
+    exit;
+}
+
 require 'functions.php';
 
 // cek apakah tombol submit ditekan
@@ -16,6 +23,9 @@ if (isset($_POST["login"])) {
         // cek password
         $row = mysqli_fetch_assoc($result);
         if (password_verify($password, $row["password"])) {
+            // set session
+            $_SESSION["login"] = true;
+
             header("Location: index.php");
             exit;
         }
